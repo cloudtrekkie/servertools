6a9b2ec96c1539743b3db4971c90c41db523a35e
Micha Grandel <m.grandel@musik-meyer.de>
2019-01-16 13:26:19 +0100

    add plugin system for server tools
    each plugin represents one type of server and provides its own logic to identify the server. in the future, each plugin may provide fixes to solve problems on this server.

--- scripts/server_tools.py
+++ scripts/server_tools.py
@@ -36,6 +36,7 @@
 #                     -----------------------------------------------------------------
 #                     @formatter:on
 
+""" test the server environment and apply fixes """
 
 from __future__ import print_function, division, unicode_literals
 import sys
@@ -50,15 +51,14 @@ from argparse import ArgumentParser
 import pkg_resources
 
 from typing import AnyStr, List
+from yapsy.PluginManager import PluginManager
+from yapsy.PluginInfo import PluginInfo
 
+# sys.path.append(os.path.abspath('../servertools'))
 
-sys.path.append('../servertools')
+from servertools.plugins import Server
+from servertools.appdirs import AppDirs
 
-from servertools import MittwaldServer, Uberspace
-from servertools.appdirs import user_log_dir, user_config_dir
-
-
-""" test the server environment and apply fixes """
 
 __status__ = 'development'
 __author__ = 'Micha Grandel'
@@ -102,7 +102,7 @@ def _log_default_handler(proc: AnyStr, log_dir: AnyStr = '.'):
     return handlers
 
 
-def main(args: List[AnyStr] = None) -> None:
+def main(args: List[AnyStr] = None) -> int:
     """
     prepare and start application
 
@@ -117,21 +117,18 @@ def main(args: List[AnyStr] = None) -> None:
         args = sys.argv
     
     script_name = os.path.basename(sys.argv[0])
-    logging_path = user_log_dir(os.path.splitext(script_name)[0], __author__, __version__)
-    config_path = user_config_dir(os.path.splitext(script_name)[0], __author__, __version__)
+    application = AppDirs(os.path.splitext(script_name)[0], appauthor=__author__, version=__version__, roaming=True)
+    logging_path = application.user_data_dir
+    config_path = application.user_config_dir
     
     # set locale
     locale.setlocale(locale.LC_ALL, '')
     _locale = locale.getlocale(locale.LC_ALL)[0]
-    locale_path = pkg_resources.resource_filename("servertools", "data/i18n".format(_locale))
+    locale_path = pkg_resources.resource_filename("servertools", "data/i18n")
     
     translate = gettext.translation("servertools", localedir=locale_path, languages=[locale.getlocale()[0], 'en'])
     translate.install()
-    if '_' not in globals():
-        try:
-            _ = unicode
-        except NameError:
-            _ = str
+    _ = lambda s: s if '_' not in globals() else _
     
     # setup logging
     handlers = _log_default_handler(proc=script_name, log_dir=logging_path)
@@ -156,14 +153,36 @@ def main(args: List[AnyStr] = None) -> None:
     command_parsers = parser.add_subparsers(help="commands")
     identify_parser = command_parsers.add_parser("identify", add_help=False, parents=[parser])
     
-    # todo: valid values for --look-for should come from installed plugins
+    # load plugins for different server types
+    plugins_directories = [
+        pkg_resources.resource_filename("servertools", "plugins"),
+        os.path.join(application.user_data_dir, 'plugins'),
+        os.path.expanduser(os.path.normpath('~/.{appname}/plugins'.format(appname=application.appname)))
+    ]
+    sys.path.extend(plugins_directories)
+    plugin_manager = PluginManager(directories_list=plugins_directories, plugin_info_ext='plugin')
+    plugin_manager.setCategoriesFilter({"Server": Server, })
+    plugin_manager.locatePlugins()
+    plugin_manager.collectPlugins()
+    
+    # plugins to identify servers and fix problems that might happen on them
+    plugins = plugin_manager.getPluginsOfCategory('Server')  # type: List[PluginInfo]
+    
+    # choices for the command line flag `--look-for`
+    server_choices = []  # type: List[AnyStr]
+    # set valid values for --look-for from installed plugins
+    for plugin in plugins:
+        commandline_argument = plugin.name.lower().replace('server', '').strip().replace(' ', '-')
+        if len(commandline_argument) > 0:
+            server_choices.append(commandline_argument)
+        else:
+            raise ValueError(_("Plugin {name} doesn't have a valid name!").format(name=plugin.name))
+    
     identify_parser.add_argument("--look-for", metavar="SERVER", dest="look_for", help=_("look for a specific server"),
-                                 choices=["mittwald", "uberspace"], default='')
+                                 choices=server_choices, default='')
     identify_parser.set_defaults(name="identify")
     
-    # todo: add feature to fix server-specific problems
     # fix_parser = command_parsers.add_parser('fix', help=_('apply fixes for a server'))
-    
     # parse arguments
     args = parser.parse_args(args[1:])
     
@@ -180,31 +199,49 @@ def main(args: List[AnyStr] = None) -> None:
         3: logging.DEBUG  # 10
     }.get(verbosity, logging.WARNING)
     stream_handler.setLevel(logging_level)
+    verbose = logger.info if verbosity > 1 else logger.debug
     
     # start application
-    logger.debug(_("{0} started at {1:%X} on Python {2} ({3})").format(
-        script_name, datetime.now(), platform.python_version(), platform.architecture()[0]
+    logger.debug(_("{program} started at {started_at:%X} on Python {py_version} ({architecture})").format(
+        program=script_name, started_at=datetime.now(), py_version=platform.python_version(),
+        architecture=platform.architecture()[0]
     ))
-    logger.debug(_("Save logs to {}").format(logging_path))
-    logger.debug(_("Load configurations from {}").format(config_path))
-    logger.debug(_("Verbosity Level at {}").format(verbosity))
+    logger.debug(_("Verbosity Level at {verbosity}").format(verbosity=verbosity))
     
-    mw = MittwaldServer(logger=logger, verbose=verbosity)
-    uberspace = Uberspace(logger=logger, verbose=verbosity)
+    plugins_information = {}
+    for category in plugin_manager.getCategories():
+        plugins_information[category] = list()
+        for plugin in plugin_manager.getPluginsOfCategory(category):
+            plugins_information[category].append({'name': plugin.name, 'author': plugin.author})
     
-    # todo: make server identification extendable through plugins for identification
-    try:
-        # handle command line subparser "identify"
-        if args.look_for == "mittwald":
-            mw.identify()
-        if args.look_for == "uberspace":
-            uberspace.identify()
-    except AttributeError:
-        # handle default behaviour
-        if not mw.identify() and not uberspace.identify():
-            logger.info(_("unknown platform"))
+    # if verbosity > 1:
+    logger.info(_("Save logs to {path}").format(path=logging_path))
+    logger.info(_("Load configurations from {path}").format(path=config_path))
+    logger.info(_('Look for plugins in:\n\t{path}').format(path=',\n\t'.join(plugins_directories)))
+    verbose(_('Include {amount} Plugin Categories.').format(amount=len(plugin_manager.getCategories())))
+    logger.debug(', '.join(plugin_manager.getCategories()))
+    verbose(_('{amount} Plugin(s) loaded.').format(amount=len(plugins_information.keys())))
+    
+    for category in plugins_information:
+        logger.debug('[{}]'.format(category))
+        for cat in plugins_information[category]:
+            logger.debug(_('\t{name} (by {author})').format(name=cat['name'], author=cat['author']))
+        logger.debug('')
+    
+    for plugin in plugins:
+        logger.debug(_("Try to identify {name}").format(name=plugin.name))
+        command = plugin.name.lower().replace('server', '').strip().replace(' ', '-'),
+        if args.look_for == command and plugin.plugin_object.identify():
+            logger.debug(_("Successfully identified {name}").format(name=plugin.name))
+            print("{} - {}".format(plugin.details.get('Server Info', 'Host'),
+                                   plugin.details.get('Server Info', 'Slogan')))
+            break
+    else:
+        print(_('Not {}').format(args.look_for) if len(args.look_for) > 0 else _('Unknown platform'))
+        return 1
     logger.debug("-" * 80)
+    return 0
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    sys.exit(main(sys.argv))
--- servertools/__init__.py
+++ servertools/__init__.py
@@ -11,8 +11,9 @@ import os
 import platform
 import logging
 from typing import List, AnyStr, Any
-
+from servertools.plugins import Server
 _ = lambda s: s
+
 
 class LoggableObject(object):
     """
@@ -30,99 +31,9 @@ class LoggableObject(object):
             self.logger.addHandler(logging.StreamHandler())
             self.logger.handlers[-1].setLevel(logging.WARNING)
         self._verbose = kwargs.get('verbose', 0)
-
-
-class Server(LoggableObject):
-    """ applies fixes for a server """
-
-    def __init__(self, logger: logging.Logger = None, **kwargs: Any) -> None:
-        """
-        :param logger: logger instance
-        """
-        super().__init__(logger, **kwargs)
-
-    def apply(self, fix: AnyStr = None, force: bool = False) -> None:
-        """
-        apply fixes for a server
-
-        :param fix: fix to apply; apply all if fix is None
-        :param force: force to apply fix even if the script is running on a different server
-        :param **kwargs: additional options
-        """
-        self._fix_server(fix=fix, force=force)
-
-    def identify(self) -> None:
-        """
-        Identify a server of a category
-
-        :return: True, if it is a server of a specified category, else False
-        """
-        pass
-
-
-class MittwaldServer(Server):
-    """ applies fixes for Mittwald server """
-
-    def __init__(self, logger: logging.Logger = None, **kwargs: Any) -> None:
-        """
-
-        :param logger: logger object
-        :type logger: :py:class:`~.logging.Logger`
-        """
-        super().__init__(logger, **kwargs)
-        self._pythonpath = '~/files/.local/lib/site-packages'
-
-    def apply(self, fix: AnyStr = None, force: bool = False) -> None:
-        """
-        apply fixes for a Mittwald server
-
-        :param fix: fix to apply; apply all if fix is None
-        :param force: force to apply fix even if the script is running on a different server
-        :param **kwargs: additional options
-        """
-        # make sure, the ~/files/.local/lib/site-packages exists!
-        if not self.identify():
-            if not force:
-                raise RuntimeWarning(_('To apply fixes for Mittwald to a foreign server, use the force-Parameter!'))
-            else:
-                self.logger.warning(_('Warning: Apply fixed for Mittwald to a foreign server!'))
-
-        if os.path.isdir(os.path.expanduser(self._pythonpath)):
-            os.makedirs(self._pythonpath)
-
-        sys.path.append(os.path.expanduser(self._pythonpath))
-
-    def identify(self) -> bool:
-        """
-        returns True, if running on a Mittwald Server
-
-        :return: whether or not running on a Mittwald Server
-        :rtype: bool
-        """
-        if self._verbose > 2:
-            self.logger.info('Verifying Mittwald ...')
         
-        mw = True if 'gentoo-mw' in platform.platform() else False
-        if mw:
-            msg = _("Can't find {}!")
-            assert (os.path.isdir(os.path.expanduser('~/backup'))), msg.format("$HOME/backup")
-            assert (os.path.isdir(os.path.expanduser('~/bin'))), msg.format("$HOME/bin")
-            assert (os.path.isdir(os.path.expanduser('~/files'))), msg.format("$HOME/files")
-            assert (os.path.isdir(os.path.expanduser('~/home'))), msg.format("$HOME/home")
-            assert (os.path.isdir(os.path.expanduser('~/html'))), msg.format("$HOME/html")
-            assert (os.path.isdir(os.path.expanduser('~/logs'))), msg.format("$HOME/logs")
-            try:
-                os.mkdir(os.path.expanduser('~/.test'))
-                mkdir = True
-            except PermissionError as e:
-                mkdir = False
-            assert (not mkdir), _("On Mittwald servers, you cannot create directories in the user's home directory!")
-            if self._verbose > 1:
-                self.logger.info("Mittwald - Webhosting. Einfach intelligent.")
-        else:
-            if self._verbose > 1:
-                self.logger.info('Not Mittwald!')
-        return mw
+
+
 
 
 class Uberspace(Server):
